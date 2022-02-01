f6a0491514016c7cf630e53d0681c095c2cf4469
lxlghostlylxl <xcyberechox@gmail.com>
2022-01-31 21:12:24 -0600

    Added SumEvenNumbers

---
+++ SumEvenNumbers.java
@@ -0,0 +1,36 @@
+public class SumEvenNumbers {
+    public static void main(String[] args) {
+       System.out.println("Total sum: " + returnSumOfEvensinRange(1, 200)); ;
+    }
+
+
+    public static int returnSumOfEvensinRange(int start, int evensToAdd){
+        int sum = 0;
+        int index = start;
+        int count = 0;
+
+        while(true){
+           
+            if(isEven(index)){
+                System.out.println("Found an even number " + index + " adding to sum");
+                sum += index;;
+                count++;
+            }else{
+                System.out.println("Odd number found: " + index);
+            }
+            index++;
+            if(count > evensToAdd){
+                break;
+            }
+        }
+        return sum;
+    }
+
+    public static boolean isEven(int num){
+        if(num % 2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
