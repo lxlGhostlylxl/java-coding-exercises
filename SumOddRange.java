243f441c344acdba8dcdda7c921589d21c6fe71d
lxlghostlylxl <xcyberechox@gmail.com>
2022-01-31 20:33:37 -0600

    Finished

---
+++ SumOddRange.java
@@ -0,0 +1,30 @@
+public class SumOddRange {
+    
+    public static boolean isOdd(int n){
+        if(n < 1){
+            return false;
+        }else{
+            if(n % 2 == 0){
+                return false;
+            }else{
+                return true;
+            }
+        }
+    }
+    
+    public static int sumOdd(int start, int end){
+        int sum = 0;
+        
+         if(start > end || start < 0 || end < 0){
+            return -1;
+        }
+    
+        for(int i = start; i < (end + 1); i++){
+            if(isOdd(i)){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+    
+    }
--- isOdd.java
+++
@@ -1,5 +0,0 @@
-class isOdd{
-public static void main(String[] args) {
-    
-}
-}
