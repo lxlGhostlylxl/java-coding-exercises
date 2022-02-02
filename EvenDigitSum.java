039bed8e5bf9cf7e19f532cf5b18b224f78a5fe4
lxlghostlylxl <xcyberechox@gmail.com>
2022-02-01 20:36:22 -0600

    init

---
+++ EvenDigitSum.java
@@ -0,0 +1,26 @@
+public class EvenDigitSum {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(1234));
+    }
+
+    public static int getEvenDigitSum(int number) {
+
+        if (number < 0) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        while (number > 0) {
+
+            if ((number % 10) % 2 == 0) {
+                sum += number % 10;
+            }
+            number /= 10;
+        }
+
+        return sum;
+
+    }
+
+}
