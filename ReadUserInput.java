fb1d2c5966894e9827fa863c8893574593142d74
lxlghostlylxl <xcyberechox@gmail.com>
2022-03-11 21:28:49 -0600

    updated

--- ReadUserInput.java
+++ ReadUserInput.java
@@ -3,14 +3,29 @@ import java.util.Scanner;
 public class ReadUserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("What is your birth year?");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine();
 
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
+        boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Hi my name is " + name + " and I was born in " + yearOfBirth);
+        if(hasNextInt){
+            
+            int age = 2022 - scanner.nextInt();
+
+            if(age > 0 && age <=100){
+                scanner.nextLine();
+
+                System.out.println("What is your name?");
+                String name = scanner.nextLine();
+                System.out.println("Hi my name is " + name + " my age is " + age );
+            }else{
+                System.out.println("Invaild age");
+            }
+        }else{
+            System.out.println("Invaild data type");
+        }
+
+      
         scanner.close();
     }
 }
